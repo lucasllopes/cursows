b718980ffe16222ecb4aca0adab92f5f1786c229
Lucas Lopes <lucas.llopes99@gmail.com>
2020-02-09 20:35:40 -0300

    Criando projeto

---
+++ src/main/java/com/lucas/cursows/CursowsApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.cursows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursowsApplication.class, args);
+	}
+
+}
